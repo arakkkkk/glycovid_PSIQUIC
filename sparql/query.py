b71977d466826a52211f0afecbc93e7e30069dae
kinoshitalab <kouiti@arakkk.local>
2022-02-21 15:03:42 +0900

    Jio

--- sparql/query.py
+++ sparql/query.py
@@ -4,13 +4,13 @@ from glob import glob
 from SPARQLWrapper import JSON, SPARQLWrapper
 
 
-def sparql(endpoint:str, sparqlist_dir:str):
+def sparql(endpoint: str, sparqlist_dir: str):
     sparql = SPARQLWrapper(endpoint)
-    dir_list = glob(sparqlist_dir+"/**.txt" ,recursive=True)
+    dir_list = glob(sparqlist_dir + "/**.txt", recursive=True)
     with open(sparqlist_dir + "/../out/out.md", "w") as f:
         f.write("")
     for i in range(len(dir_list)):
-        print(str(i+1)+"/"+str(len(dir_list)), "\texecute: ", dir_list[i])
+        print(str(i + 1) + "/" + str(len(dir_list)), "\texecute: ", dir_list[i])
         with open(dir_list[i]) as f:
             query = f.read()
         sparql.setQuery(query)
@@ -18,6 +18,7 @@ def sparql(endpoint:str, sparqlist_dir:str):
         with open(sparqlist_dir + "/../out/out.md", "a") as f:
             f.write("\n--- \n")
             f.write("## SPARQL query\n")
+            f.write(dir_list[i].split("/")[-1].split(".")[0] + "\n")
             f.write("```\n")
             f.write(query)
             f.write("```\n")
@@ -41,7 +42,8 @@ def sparql(endpoint:str, sparqlist_dir:str):
         except Exception as e:
             print(e)
 
+
 if __name__ == "__main__":
     endpoint = "http://localhost:3030/psicquic_2_15_22/sparql"
-    sparqlist_dir = "/Users/kouiti/localfile/glycovid_PSIQUIC/sparql/sparqlist"
+    sparqlist_dir = "/Users/kouiti/localfile/glycovid/glycovid_PSIQUIC/sparql/sparqlist"
     sparql(endpoint, sparqlist_dir)
